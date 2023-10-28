de8ee51ab9226510e6d80bc191e5020a312b0948
enzo0989 <enzodeveloper01@gmail.com>
2023-10-28 09:18:35 -0300

    Pseudo code
    To add some guide for myself

---
+++ main.py
@@ -0,0 +1,10 @@
+#Pseudo code
+
+# Prompts for a link of a playlist
+# If the link is a valid playlist(find a way to determine this):
+# Open youtube (login if necessary)
+# Search the playlist
+# Find the link of all the songs inside the playlist
+# Find a way to download each video as an audio file
+# Put them in a folder 
+# End
