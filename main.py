40ca4c1a1bb22b6ff67560a04f22e4023e677060
enzo0989 <enzodeveloper01@gmail.com>
2023-10-29 19:23:15 -0300

    deleted pseudo code

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import re
+import BeautifulSoup
 #Pseudo code
 
 def main():
     # Prompts for a url to a playlist
     url = input("link to a playlist: ")
     if playlist_validator(url):
-        print("valid")
+        url_searcher(url)
     else:
         print("not valid")
-
 
 
 def playlist_validator(url):
@@ -19,13 +18,10 @@ def playlist_validator(url):
         return False
 
 
-# If the link is a valid playlist(find a way to determine this):
-# Open youtube (login if necessary)
-# Search the playlist
-# Find the link of all the songs inside the playlist
-# Find a way to download each video as an audio file
-# Put them in a folder 
-# End
+
+
+
+
 
 
 if __name__ == "__main__":
