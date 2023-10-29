6391a3473c7af5440fc4aa094a94f55d59c0cfb2
enzo0989 <enzodeveloper01@gmail.com>
2023-10-28 23:18:41 -0300

    added url related function
    to validate playlist urls

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import re
 #Pseudo code
 
-# Prompts for a link of a playlist
+def main():
+    # Prompts for a url to a playlist
+    url = input("link to a playlist: ")
+    if playlist_validator(url):
+        print("valid")
+    else:
+        print("not valid")
+
+
+
+def playlist_validator(url):
+    pattern = r"^http(s)?://www.youtube.com/(watch\?v|playlist\?list)=[\w\-\_]+"
+    if matches := re.match(pattern, url):
+        return True
+    else:
+        return False
+
+
 # If the link is a valid playlist(find a way to determine this):
 # Open youtube (login if necessary)
 # Search the playlist
@@ -8,3 +26,7 @@
 # Find a way to download each video as an audio file
 # Put them in a folder 
 # End
+
+
+if __name__ == "__main__":
+    main()
